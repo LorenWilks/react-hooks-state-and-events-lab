04119e7daaca733e0600912e906a720e0f31447a
Loren Wilks <Loren.wilks@gmail.com>
2022-07-22 10:38:07 -0600

    commit tree d3b47e1c762c8e367f070fa02183051cab72f0c5

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [notInCart, setNotInCart] = useState(true)
+
+    function toggleCart(){
+      setNotInCart((notInCart) => !notInCart)
+    }
+
+  const cartClass = notInCart ? "" : "in-cart"
+
   return (
-    <li className="">
+    <li className={cartClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={toggleCart}>{notInCart ? "Add to Cart" : "Remove From Cart"}</button>
     </li>
   );
 }
